c66cb8626ad63eb72293765093b0374f0cba8317
Anapher (Vincent) <anapher@protonmail.com>
2017-07-13 20:34:17 +0200

    make CheckPermissions async and add documentation

--- CodeElements.NetworkCallTransmissionProtocol/IEventSubscriber.cs
+++ CodeElements.NetworkCallTransmissionProtocol/IEventSubscriber.cs
@@ -7,7 +7,16 @@ namespace CodeElements.NetworkCallTransmissionProtocol
     /// </summary>
     public interface IEventSubscriber
     {
-        bool CheckPermissions(int[] permissions, object parameter);
+        /// <summary>
+        ///     Check the permissions for an <see cref="IEventSubscriber" /> to receive an event
+        /// </summary>
+        /// <param name="permissions">
+        ///     The permissions which were specified in the <see cref="EventPermissionsAttribute" /> of the
+        ///     event. Null if the event doesn't has that attribute.
+        /// </param>
+        /// <param name="parameter">The parameter (args) of the event.</param>
+        /// <returns>Return a boolean indicating whether the event should be triggered (with <see cref="TriggerEvent" />) or not</returns>
+        Task<bool> CheckPermissions(int[] permissions, object parameter);
 
         /// <summary>
         ///     Trigger an event on the subscriber
