beb620a173cd32fe209835cc4277c35a43b4bb17
Anapher (Vincent) <anapher@protonmail.com>
2017-09-06 17:14:38 +0200

    Fix bug when .Net Core and .Net FW communicate

--- CodeElements.NetworkCallTransmission/Extensions/ReflectionIdExtensions.cs
+++ CodeElements.NetworkCallTransmission/Extensions/ReflectionIdExtensions.cs
@@ -8,15 +8,26 @@ namespace CodeElements.NetworkCallTransmission.Extensions
     {
         public static uint GetMethodId(this MethodInfo methodInfo)
         {
-            return MurmurHash.Hash(methodInfo.Name + methodInfo.ReturnParameter?.ParameterType.FullName +
-                                   string.Join("",
-                                       methodInfo.GetParameters()
-                                           .Select(x => x.Position.ToString() + x.ParameterType.FullName)));
+            var stringValue = methodInfo.Name + GetInvariantFullName(methodInfo.ReturnParameter?.ParameterType) +
+                              string.Join("",
+                                  methodInfo.GetParameters()
+                                      .Select(x => x.Position.ToString() + GetInvariantFullName(x.ParameterType)));
+            return MurmurHash.Hash(stringValue);
         }
 
         public static ulong GetEventId(this EventInfo eventInfo, Type interfaceType, uint sessionId)
         {
-            return MurmurHash.Hash(eventInfo.Name + eventInfo.EventHandlerType.FullName + interfaceType.Name) << 32 | sessionId;
+            return (MurmurHash.Hash(eventInfo.Name + eventInfo.EventHandlerType.FullName + interfaceType.Name) << 32) |
+                   sessionId;
+        }
+
+        private static string GetInvariantFullName(Type type)
+        {
+            if (type == null)
+                return null;
+            return
+                type.Name + type
+                    .Namespace; //no assembly because .net Core and .Net Framework have different assembly names (e. g. mscorlib vs CoreLib)
         }
     }
 }
